2422237dbf1d6156aa6b3663e69f7771a11c476d
1jgabriel <gsantana152@gmail.com>
2025-01-02 15:34:37 -0300

    BookListScreen UI
    Implemented the UI for the BookListScreen, including:
    
    - BookListItem composable for displaying individual book items.
    - BookList composable for displaying a list of books.
    - PulseAnimation for loading placeholder.
    - BookListScreen composable with search bar, tab layout (Search Results & Favorites), and displays book lists based on the selected tab.
    - Added strings for search results, favorites, and empty states.
    - Updated previews to showcase the BookListScreen.

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -11,6 +11,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.booklist.BookListScreen
+import com.plcoding.bookpedia.book.presentation.booklist.BookListState
 import com.plcoding.bookpedia.book.presentation.booklist.components.BookSearchBar
 
 @Preview
@@ -31,4 +34,31 @@ private fun BookSearchBarPreview() {
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Philipp Lackner"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.67854,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResult = books
+        ),
+        onAction = {}
+    )
 }
