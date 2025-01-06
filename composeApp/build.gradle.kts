3912c7e9978fa5b4541967bd69166b6ddef54141
1jgabriel <gsantana152@gmail.com>
2025-01-06 11:41:24 -0300

    feat: Display initial books and add internet permission
    Added internet permission to the AndroidManifest.xml.
    Initialized the BookListState with a list of 100 sample books.
    Updated desktop target formats to include Exe.
    Adjusted the size of the book_error_2 drawable.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -122,7 +122,7 @@ compose.desktop {
         mainClass = "com.plcoding.bookpedia.MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
             packageName = "com.plcoding.bookpedia"
             packageVersion = "1.0.0"
         }
